71df1479d98bb37e3f91fe4f539618a32fd4fab2
Freek Dijkstra <freek@mbp-freek.science.uva.nl>
2008-10-28 13:33:56 +0100

    - Use Foundation.NSDictionary to read preference file   this merges the branch with the latest version of Latex.tmbundle

--- Support/bin/tmprefs.py
+++ Support/bin/tmprefs.py
@@ -1,11 +1,37 @@
 import os
 import newplistlib as plistlib
+import string
 
 # NIB constants:
 # latexTypesetAction radio buttons
 typesetActionLatex   = 0
 typesetActionMake    = 1
 typesetActionLatexmk = 2
+
+try:
+    import Foundation
+    haspyobjc = True
+except:
+    haspyobjc = False
+
+
+try:
+    from subprocess import Popen
+except:
+    PIPE = 1
+    STDOUT=1
+    class Popen(object):
+        """Popen:  This class provides backward compatibility for Tiger
+           Do not assume anything about this works other than access
+           to stdin, stdout and the wait method."""
+        def __init__(self, command, **kwargs):
+            super(Popen, self).__init__()
+            self.command = command
+            self.stdin, self.stdout = os.popen4(command)
+
+        def wait(self):
+            stat = self.stdout.close()
+            return stat
 
 class Preferences(object):
     """docstring for Preferences"""
@@ -19,14 +45,15 @@ class Preferences(object):
             'latexTypesetAction': typesetActionLatex,
             'latexUselatexmk' : 0,
             'latexViewer' : "TextMate",
-            'latexKeepLogWin' : 1
+            'latexKeepLogWin' : 1,
+            'latexDebug' : 0,
         }
         self.prefs = self.readPrefs()
-    
+        
     def __getitem__(self,key):
         """docstring for __getitem__"""
         return self.prefs.get(key,None)
-    
+
     def readPrefs(self):
         """Read preferences from file, and set self.prefs. If a key if not defined (or empty!),
         replace it with the defaul value"""
@@ -60,53 +87,32 @@ class Preferences(object):
         # plutil apparently tries to do something to /dev/stdout which causes an error message to be appended
         # to the output.
         #
-        # "defaults read com.macromates.textmate latexVerbose" does this, but can't distinguish between 1 and True.
         plDict = {}
-        os.system("plutil -convert xml1 $HOME/Library/Preferences/com.macromates.textmate.plist -o /tmp/tmltxprefs1.plist")
-        os.system(" cat /tmp/tmltxprefs1.plist | tr -d '\\000'-'\\011''\\013''\\014''\\016'-'\\037''\\200'-'\\377' > /tmp/tmltxprefs.plist" )
-        pl = open('/tmp/tmltxprefs.plist')
-        try:
-            plDict = plistlib.readPlist(pl)
-        except:
-            print '<p class="error">There was a problem reading the preferences file, continuing with defaults</p>'
-        pl.close()
-        try:
-            os.remove("/tmp/tmltxprefs.plist")
-            os.remove("/tmp/tmltxprefs1.plist")
-        except:
-            print '<p class="error">Problem removing temporary prefs file</p>'
-        #keys = ['latexAutoView', 'latexEngine', 'latexEngineOptions', 'latexUselatexmk', 'latexViewer']
-        #for key in keys:
-        #    value = self.readTMPref(key)
-        #    if value != None:
-        #        plDict[key] = value
+        if haspyobjc:
+            plDict = Foundation.NSDictionary.dictionaryWithContentsOfFile_(os.environ["HOME"]+"/Library/Preferences/com.macromates.textmate.plist")
+        else:   # TODO remove all this once everyone is on leopard
+            os.system("plutil -convert xml1 \"$HOME/Library/Preferences/com.macromates.textmate.plist\" -o /tmp/tmltxprefs1.plist")
+            null_tt = "".join([chr(i) for i in range(256)])
+            non_printables = null_tt.translate(null_tt, string.printable)
+            plist_str = open('/tmp/tmltxprefs1.plist').read()
+            plist_str = plist_str.translate(null_tt,non_printables)
+            try:
+                plDict = plistlib.readPlistFromString(plist_str)
+            except:
+                print '<p class="error">There was a problem reading the preferences file, continuing with defaults</p>'
+            try:
+                os.remove("/tmp/tmltxprefs1.plist")
+            except:
+                print '<p class="error">Problem removing temporary prefs file</p>'
         return plDict
-    
-    def readTMPref(self, key):
-        domain = "com.macromates.textmate"
-        stdin,stdout = os.popen2('defaults read %s "%s"' % (domain, key))
-        value = stdout.read()
-        stdin.close()
-        stdout.close()
-        if value == "":
-            value = None
-        return value
-    
-    def extractLatexPrefs(self):
-        """plist-formated string with preference values"""
-        prefs = {}
-        for key, value in self.prefs.iteritems():
-            if key.startswith("latex"):
-                prefs[key] = value
-        return prefs
-
+        
     def toDefString(self):
-        """plist-formated string with default values"""
+        """plist-formated string with preference values"""
         instr = plistlib.writePlistToString(self.defaults)
-        stdin,stdout = os.popen2('pl')
-        stdin.write(instr)
-        stdin.close()
-        defstr = stdout.read()
+        runObj = Popen('pl',shell=True,stdout=PIPE,stdin=PIPE,stderr=STDOUT,close_fds=True)
+        runObj.stdin.write(instr)
+        runObj.stdin.close()
+        defstr = runObj.stdout.read()
         return defstr.replace("\n","")
 
 if __name__ == '__main__':
@@ -114,4 +120,4 @@ if __name__ == '__main__':
     print "Preferences =", test.prefs
     print "Defaults (plist) =", repr(test.toDefString())
     
-    
+
